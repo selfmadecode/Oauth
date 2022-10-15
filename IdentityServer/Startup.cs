c3bd0f9015837bd8bad1aa27d4ac47b78665a01e
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2022-10-16 00:09:17 +0100

    updated startup

--- IdentityServer/Startup.cs
+++ IdentityServer/Startup.cs
@@ -20,10 +20,10 @@ namespace IdentityServer
         {
             services.AddIdentityServer()
                 .AddInMemoryClients(Config.Clients)
-                .AddInMemoryIdentityResources(Config.IdentityResources)
-                .AddInMemoryApiResources(Config.ApiResources)
+                //.AddInMemoryIdentityResources(Config.IdentityResources)
+                //.AddInMemoryApiResources(Config.ApiResources)
                 .AddInMemoryApiScopes(Config.ApiScopes)
-                .AddTestUsers(Config.TestUsers)
+               // .AddTestUsers(Config.TestUsers)
                 .AddDeveloperSigningCredential();
             // To test if identityserver is up, navigate to this url
             //Get - https://localhost:5005/.well-known/openid-configuration
