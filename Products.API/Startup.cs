4cdcd7d5f4d2789ad8c79c4b9f8ec0b6db4d1a90
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2022-10-16 01:06:23 +0100

    Added Identity

--- Products.API/Startup.cs
+++ Products.API/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
@@ -32,6 +33,16 @@ namespace Products.API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Products.API", Version = "v1" });
             });
+
+            services.AddAuthentication("Bearer")
+                    .AddJwtBearer("Bearer", options =>
+                    {
+                        options.Authority = "https://localhost:5005";
+                        options.TokenValidationParameters = new TokenValidationParameters
+                        {
+                            ValidateAudience = false
+                        };
+                    });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,6 +59,7 @@ namespace Products.API
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
